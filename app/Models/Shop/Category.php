4f9f5747f0cc19e3f4cfe9c3fa4196336d390a22
devphpru <devphpru@yandex.ru>
2024-04-26 08:56:19 +0300

    dev 2 feat (Добавил модель category)

---
+++ app/Models/Shop/Category.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models\Shop;
+
+use Illuminate\Database\Eloquent\Builder;
+
+/**
+ * Class Category
+ *
+ * @property int $id ID
+ * @property int $parent_id ID родителя
+ * @property string $slug ЧПУ
+ * @property string $name Название
+ * @property string $content Описание
+ * @property string $thumbnail Изображение
+ * @property bool $is_active Активность
+ * @property \Illuminate\Support\Carbon|null $created_at Дата создания
+ * @property \Illuminate\Support\Carbon|null $updated_at Дата обновления
+ *
+ * @mixin Builder
+ * @package App\Models\Shop
+ */
+class Category extends BaseModel
+{
+    protected $fillable = [
+        'parent_id',
+        'slug',
+        'name',
+        'content',
+        'thumbnail',
+        'is_active',
+    ];
+
+    protected $casts = [
+        'is_active' => 'boolean',
+    ];
+
+}
---
+++ database/factories/Shop/CategoryFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories\Shop;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Shop\Category>
+ */
+class CategoryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        static $i = 0;
+        $name = fake()->unique()->words(mt_rand(3, 6), true);
+
+        return [
+            'parent_id' => (++$i > 4) ? mt_rand(1, 4) : 0,
+            'slug' => Str::slug($name),
+            'name' => $name,
+            'content' => fake()->realText(mt_rand(150, 200)),
+            'thumbnail' => '',
+            'is_active' => true,
+        ];
+    }
+}
