ff4eac2db701a98665d5a56a86e3ea8cf8b192e0
devphpru <devphpru@yandex.ru>
2024-04-26 13:30:23 +0300

    dev 4 feat (Вывел товары в категории)

--- resources/views/shops/categories/single.blade.php
+++ resources/views/shops/categories/single.blade.php
@@ -11,6 +11,27 @@
         </div>
         <div class="col-md-8">
             <h2>{{ $title }}</h2>
+            @if ($paginator->isNotEmpty())
+                <nav class="row">
+                    @foreach ($paginator as $value)
+                        <div class="col-md-4 mb-2">
+                            <div class="card">
+                                <img src="{{ $value->thumbnail }}" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <h5 class="card-title">{{ __($value->title) }}</h5>
+                                    <p class="card-text">{{ $value->category->name }}</p>
+                                    <p class="card-text">{{ $value->price }} руб.</p>
+                                    <a href="#" class="btn btn-primary">В корзину</a>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                    <nav aria-label="Page navigation example">
+                        <ul class="pagination">
+                            {{ $paginator->links('vendor.pagination.bootstrap-4') }}
+                        </ul>
+                    </nav>
+            @endif
         </div>
     </div>
 @endsection
--- tests/Feature/Shop/CategoryControllerTest.php
+++ tests/Feature/Shop/CategoryControllerTest.php
@@ -2,9 +2,12 @@
 
 namespace Tests\Feature\Shop;
 
+use App\Models\Shop\Brand;
 use App\Models\Shop\Category;
+use App\Models\Shop\Product;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use App\Enums\FieldStatus;
 use Tests\TestCase;
 
 class CategoryControllerTest extends TestCase
@@ -14,35 +17,74 @@ class CategoryControllerTest extends TestCase
     public function testGetViewCategoryIndex(): void
     {
         $this->categoryFactory();
+        $this->brandFactory();
+        $this->productFactory();
         $title = __('Каталог товаров');
+        $perPage = 10;
 
         $response = $this->get(route('shop.categories.index'));
+        $products = Product::with('category')
+            ->where('is_active', '=', FieldStatus::TURN_ON->value)
+            ->orderByDesc('id')
+            ->paginate($perPage)
+            ->withQueryString();
 
         $response->assertSuccessful();
         $response->assertViewIs('shops.categories.index');
         $response->assertViewHas([
             'title' => $title,
+            'paginator' => $products,
         ]);
     }
 
     public function testGetViewCategorySingle(): void
     {
         $this->categoryFactory();
+        $this->brandFactory();
+        $this->productFactory();
         $item = Category::get()->first();
         $title = __('Категория: ' . $item->name);
+        $perPage = 10;
 
         $response = $this->get(route('shop.categories.single', $item));
+        $products = Product::with('category')
+            ->where('is_active', '=', FieldStatus::TURN_ON->value)
+            ->where('category_id', '=', $item->id)
+            ->orderByDesc('id')
+            ->paginate($perPage)
+            ->withQueryString();
+
+        if ($products->isEmpty()) {
+            $products = Product::with('category')
+                ->where('is_active', '=', FieldStatus::TURN_ON->value)
+                ->whereIn('category_id', function ($builder) use ($item) {
+                    $builder->select('id')->from('categories')->where('parent_id', '=', $item->id);
+            })->orderByDesc('id')
+            ->paginate($perPage)
+            ->withQueryString();
+        }
 
         $response->assertSuccessful();
         $response->assertViewIs('shops.categories.single');
         $response->assertViewHas([
             'title' => $title,
+            'paginator' => $products,
         ]);
     }
 
     private function categoryFactory(): void
     {
         Category::factory(10)->create();
+    }
+
+    private function productFactory(): void
+    {
+        Product::factory(37)->create();
+    }
+
+    private function brandFactory(): void
+    {
+        Brand::factory(5)->create();
     }
 
 }
